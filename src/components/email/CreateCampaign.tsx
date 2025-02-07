0b36a4f01eeec20ae3935aa941106704f57874b5
Yishak Kidanu <yisak04@gmail.com>
2025-02-08 00:02:54 +0300

    Added state for renaming

--- src/components/email/CreateCampaign.tsx
+++ src/components/email/CreateCampaign.tsx
@@ -4,6 +4,7 @@ import EmailForm from "./EmailForm";
 
 const CreateCampaign = () => {
   const [campaignName, setCampaignName] = useState<string>("Untitled");
+  const [editNameClicked, setEditNameClicked] = useState<boolean>(false);
   const inputRef = useRef<HTMLInputElement | null>(null);
 
   const handleCampaignNameChange = (event: string) => {
@@ -19,13 +20,14 @@ const CreateCampaign = () => {
 
   const handleRenameClick = () => {
     inputRef?.current?.focus();
+    setEditNameClicked(true);
   };
 
   return (
     <div className="">
       <div className="border-b h-14">
         <div className="mx-auto w-4/5 flex justify-end">
-          <button className="bg-black text-white px-6 py-2 rounded-3xl">
+          <button className="bg-black text-white px-6 py-2 rounded-3xl w-[10%]">
             Send
           </button>
         </div>
@@ -39,9 +41,28 @@ const CreateCampaign = () => {
             onKeyDown={handleKeyDown}
             className="font-light text-3xl"
           />
-          <button onClick={handleRenameClick} className="font-bold text-xs">
-            Edit name
-          </button>
+          {!editNameClicked ? (
+            <button onClick={handleRenameClick} className="font-bold text-xs">
+              Edit name
+            </button>
+          ) : (
+            <div className="flex space-x-2 w-[15%]">
+              <button
+                // onClick={handleRenameClick}
+                className="font-bold text-xs bg-black text-white px-6 py-2 w-full rounded-3xl"
+              >
+                Save
+              </button>
+              <button
+                onClick={() => {
+                  setEditNameClicked(false);
+                  inputRef.current?.blur()}}
+                className="font-bold text-xs text-black border border-black px-6 w-full py-2 rounded-3xl"
+              >
+                Cancel
+              </button>
+            </div>
+          )}
         </div>
       </div>
       <EmailForm />
