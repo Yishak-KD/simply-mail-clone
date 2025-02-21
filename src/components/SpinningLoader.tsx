5367ff5a89c012d8d497313f9f77bcd29d66dec5
Yishak Kidanu <yisak04@gmail.com>
2025-02-21 09:39:49 +0300

    Minor refactor

--- src/components/SpinningLoader.tsx
+++ src/components/SpinningLoader.tsx
@@ -3,7 +3,7 @@ import { Box } from "@mui/material";
 const SpinningLoader = () => {
   return (
     <Box className="fixed inset-0 flex items-center justify-center">
-      <Box className="w-8 h-8 border-4 border-transparent border-t-black rounded-full animate-spin" />
+      <Box className="w-10 h-10 border-4 border-transparent border-t-black rounded-full animate-spin" />
     </Box>
   );
 };
